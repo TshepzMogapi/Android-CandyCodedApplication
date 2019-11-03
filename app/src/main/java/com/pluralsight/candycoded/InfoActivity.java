2d1622865bfb8c1ce002d0666ef3c1414ca7ad84
TebogoMothelo <tshepzmogapi@gmail.com>
2019-11-03 10:40:23 +0200

    start phone activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -39,14 +39,23 @@ public class InfoActivity extends AppCompatActivity {
             startActivity(mapIntent);
         }
 
-//        mapIntent.resolveActivity(getPackageManager())
-
-
-
     }
     // ***
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
+    public void createPhoneIntent(View view) {
+
+        Uri uri = Uri.parse("tel:0123456789");
+
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+
+        phoneIntent.setData(uri);
+
+        startActivity(phoneIntent);
+
+
+
+    }
     // ***
 }
